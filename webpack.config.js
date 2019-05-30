7ab42d1e621d6e1c764cecae7b368af589b2aaf0
pengtao <pengtao_it@163.com>
2019-05-30 15:11:37 +0800

    特殊字符校验

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,21 @@ module.exports = {
     library: 'cableHelper',
     libraryTarget: 'umd'
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
   externals: {
     lodash: {
       commonjs: 'lodash',
